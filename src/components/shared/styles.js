25bb20ccc2569e186358fef45e4769e838824b54
Fuxa, Jan <jan.fuxa@firma.seznam.cz>
2020-03-23 21:21:25 +0100

    init storybook

--- src/components/shared/styles.js
+++ src/components/shared/styles.js
@@ -55,26 +55,23 @@ export const spacing = {
 
 export const typography = {
   type: {
-    primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
-    code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
+    primary: 'Arial, sans-serif',
   },
   weight: {
     regular: '400',
     bold: '700',
-    extrabold: '800',
-    black: '900',
   },
   size: {
-    s1: '12',
-    s2: '14',
-    s3: '16',
-    m1: '20',
-    m2: '24',
-    m3: '28',
-    l1: '32',
-    l2: '40',
+    s1: '11',
+    s2: '12',
+    s3: '13',
+    m1: '16',
+    m2: '18',
+    m3: '21',
+    l1: '24',
+    l2: '36',
     l3: '48',
-    code: '90',
+    l4: '60',
   },
 };
 
